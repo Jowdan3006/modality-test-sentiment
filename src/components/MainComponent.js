ecbae176bf9d9382e7e287c4deef1d49bfe7a1d6
Ross-ley <44468729+Ross-ley@users.noreply.github.com>
2019-09-19 11:40:41 +0100

    updated MainComponted
    I have gotten the file to send the data need to get the data so that it sends the right data.

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,38 +1,73 @@
 import React, {Component} from 'react';
 import chat1 from '../testData.json'
 
-class MainComponent extends Component {     
+class MainComponent extends Component { 
+
+    // Chat = () =>{
+    //     let regex = /(<([^>]+)>)/ig;
+    //     let chatMessages = chat1.value.map((textDataDetail , index)=>{
+    //         return <div key={index}>
+    //                     {textDataDetail.body.content.replace(regex, '')}
+    //                 </div>
+    //     })
+    //     return chatMessages
+    // }
+
     Chat = () =>{
         let regex = /(<([^>]+)>)/ig;
         let chatMessages = chat1.value.map((textDataDetail , index)=>{
-            return <div key={index}>
-                        {textDataDetail.body.content.replace(regex, '')}
-                    </div>
+            return {
+                    "language": "en",
+                    "id": index,
+                    "text": textDataDetail.body.content.replace(regex, '')
+                    }
+                    // has to be in the order id language, id then text if not in this order then api wont work.
+                   
         })
         return chatMessages
     }
-    handleSubmit(){ 
+    // ChatTitle = () =>{
+    //     let regex = /(<([^>]+)>)/ig;
+    //     let chat = chat1.value.map((wholeBody , index)=>{
+    //         return <span key={index}>
+    //                     {wholeBody}
+    //                 </span>
+    //     })
+    //     return chat
+    // }
+
+    handleSubmit(){
+        let responseDemo = {"documents": this.Chat()}; 
         fetch('https://westcentralus.api.cognitive.microsoft.com/text/analytics/v2.1/sentiment', {
-            method: 'post',
-            // need to put into header
-            // Host: westcentralus.api.cognitive.microsoft.com,
-            // Ocp-Apim-Subscription-Key: 28ee5326ee394d9d8525159edaa73b26,
-         body: {
-          "content": this.Chat()
-         }
-        });
+            method: "POST",
+            headers:{
+                "Ocp-Apim-Subscription-Key": "28ee5326ee394d9d8525159edaa73b26",
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            },
+            body: this.Chat(),
+        }).then(Response => Response.json())
+          .then(responseData =>  responseDemo);
        }; 
+       /*
+        returning bad object as this is sending [object Object] and if you look into the [object Object]
+        it displays the data as 
+        id
+        language
+        text
+        i need to it send as one item and not many object arrays. 
+       */
 
   render() {
+      console.log(this.handleSubmit());
+      console.log(this.Chat());
     return (
         <div>
             {/* {this.Chat()} */}
-            {this.handleSubmit()}
+            {/* {this.handleSubmit()} */}
         </div>
     )
   }
 }
 
 export default MainComponent;
-
-// https://westcentralus.api.cognitive.microsoft.com/text/analytics/v2.1/sentiment
